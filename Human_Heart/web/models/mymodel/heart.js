b787f615cbdb3090dbfdcc5d1fb6970da605286f
Prabhu Siddharth <prabhusiddharthr@gmail.com>
2014-12-09 18:50:41 -0800

    Penultimate Version

---
+++ Human_Heart/web/models/mymodel/heart.js
@@ -0,0 +1,33 @@
+MODELS['Heart.obj'] = {
+  materials: {
+    'wire_224086086': {
+      Ka: [229, 76, 76],
+      Kd: [229, 76, 76],
+      Ks: [76, 76, 76],
+      Ns: 10.000000,
+      d: 255
+    }
+  },
+  decodeParams: {
+    decodeOffsets: [-6009,-8191,-5517,0,0,-511,-511,-511],
+    decodeScales: [0.007369,0.007369,0.007369,0.000978,0.000978,0.001957,0.001957,0.001957]
+  },
+  urls: {
+    '77394c16.heart.utf8': [
+      { material: 'wire_224086086',
+        attribRange: [0, 55294],
+        indexRange: [442352, 106532],
+        bboxes: 1269274,
+        names: ['sub01', 'sub02'],
+        lengths: [196599, 122997]
+      },
+      { material: 'wire_224086086',
+        attribRange: [761948, 36820],
+        indexRange: [1056508, 70922],
+        bboxes: 1269286,
+        names: ['sub02', 'sub03'],
+        lengths: [73602, 139164]
+      }
+    ]
+  }
+};
---
+++ Skeleton/bin/mymodel.js
@@ -0,0 +1,40 @@
+MODELS['Skeleton.obj'] = {
+  materials: {
+    '01___default': {
+      Ka: [242, 242, 235],
+      Kd: [242, 242, 235],
+      Ks: [39, 39, 39],
+      Ns: 20.000000,
+      d: 0
+    }
+  },
+  decodeParams: {
+    decodeOffsets: [-2619,8,-1311,0,0,-511,-511,-511],
+    decodeScales: [0.000161,0.000161,0.000161,0.000978,0.000978,0.001957,0.001957,0.001957]
+  },
+  urls: {
+    'cba0f71f.Skeleton.utf8': [
+      { material: '01___default',
+        attribRange: [0, 55294],
+        indexRange: [442352, 105177],
+        bboxes: 2270450,
+        names: ['sub01', 'sub02'],
+        lengths: [196581, 118950]
+      },
+      { material: '01___default',
+        attribRange: [757883, 55294],
+        indexRange: [1200235, 102232],
+        bboxes: 2270462,
+        names: ['sub02', 'sub03', 'sub04'],
+        lengths: [77649, 196599, 32448]
+      },
+      { material: '01___default',
+        attribRange: [1506931, 54803],
+        indexRange: [1945355, 108365],
+        bboxes: 2270480,
+        names: ['sub04', 'sub05', 'skelet01'],
+        lengths: [164151, 36732, 124212]
+      }
+    ]
+  }
+};
